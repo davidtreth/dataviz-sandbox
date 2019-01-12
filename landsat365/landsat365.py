43fc67a8835fbb2239e393bf79276d2246291278
davidtreth <davidtreth@gmail.com>
2019-01-12 23:36:24 +0000

    partially opaque box behind text, improve placement of label

--- landsat365/landsat365.py
+++ landsat365/landsat365.py
@@ -11,40 +11,73 @@ from PIL import Image, ImageDraw, ImageFont, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
-def createCaption(rawimage, descript, fontsize, left=False):
+def createCaption(rawimage, descript, fontsize, left=False, drawBox=True):
 	# get a font
 	fnt = ImageFont.truetype('arial.ttf', size=fontsize)
 	desclength = len(descript)
-        # width of the box to measure to decide
-        # whether to have black or white text
-	boxwidth = int(desclength * fontsize / 1.8 + fontsize*0.75)
+    # width of the box to measure to decide
+    # whether to have black or white text	
 	xs, ys = rawimage.size
-        # caption to be at top-right of image unless left=True
-	if left:
-		box = (0,0,boxwidth,ys/12)
+	# print(xs, ys)
+    # create a new Image to contain the text overprinted
+	txtbackgcolour = (0,0,0,0)        
+	txt = Image.new('RGBA', rawimage.size, txtbackgcolour)
+	# get a drawing context
+	d = ImageDraw.Draw(txt)
+	# find the size the text will be in pixels
+	tlength, theight = d.textsize(descript, font=fnt)
+	print("text length = {l}px, height= {h}px".format(l=tlength, h=theight))
+	if "\n" in descript:
+		tmargin = 0.5*theight / len(descript.split("\n"))
 	else:
-		box = (xs-boxwidth,0,xs,ys/12)
+		tmargin = 0.5*theight
+	# make the width a bit longer than the box to leave 1/2 textheight at each end
+	boxwidth = int(tlength + 2*tmargin)	
+	# caption to be at top-right of image unless left=True		        
+	if left:
+		box = (0,0,boxwidth,theight + 2*tmargin)
+	else:
+		box = (xs-boxwidth,0,xs,theight + 2*tmargin)
+	# extract a region from the image
 	region = rawimage.crop(box)
 	r,g,b,a = region.split()
-	meanregion = numpy.mean([numpy.mean(r), numpy.mean(g), numpy.mean(b)])
+	# get band means and percentiles
+	meanregion = [numpy.mean(r), numpy.mean(g), numpy.mean(b)]
+	print("band means:")
 	print(meanregion)
-	
+	meanregion = numpy.mean(meanregion)
+	print("overall mean pixel value = {m}".format(m=meanregion))
+	percentiles = [numpy.percentile(r,[10,90]),
+	numpy.percentile(g,[10,90]),
+	numpy.percentile(b,[10,90])]
+	print("10th and 90th percentiles:")
+	print(percentiles)
+	p10 = [i[0] for i in percentiles]
+	p90 = [i[1] for i in percentiles]
+	meanp10 = numpy.mean(p10)
+	meanp90 = numpy.mean(p90)	
+	# fully opaque text and a partially opaque box
 	opac=255
-        # if the image is dark, have white text and vice versa
+	boxopac = 128
+    # if the region is dark, have white text and vice versa
+    # if the region is very dark or very light, reduce the opacity of the box
 	if meanregion < 100:
-		txtcolour = (0,0,0,0)
+		boxopac = int(boxopac * meanp90/255)
+		boxcolour = (0,0,0,boxopac)
 		txtcolour2 = (255,255,255,opac)
 	else:
-		txtcolour = (255,255,255,0)	
-		txtcolour2 = (0,0,0,opac)	
-	txt = Image.new('RGBA', rawimage.size, txtcolour)
-	# get a drawing context
-	d = ImageDraw.Draw(txt)		
+		boxopac = int(boxopac * (255-meanp10)/255)
+		boxcolour = (255,255,255,boxopac)	
+		txtcolour2 = (0,0,0,opac)		
+	if drawBox:
+		# print(box)
+		d.rectangle(box,fill=boxcolour,outline=None,width=0)
 	# draw text
 	if left:
-		d.text((fontsize*0.7,fontsize*0.7), descript, font=fnt, fill=txtcolour2)
+		d.text((tmargin,tmargin), descript, font=fnt, fill=txtcolour2)
 	else:	
-		d.text((xs-boxwidth,fontsize*0.7), descript, font=fnt, fill=txtcolour2)
+		d.text((xs-boxwidth + tmargin, tmargin), descript, font=fnt, fill=txtcolour2)
+	# overlay on the original image		
 	out = Image.alpha_composite(rawimage, txt)	
 	return out
 
@@ -66,6 +99,7 @@ soup = BeautifulSoup(raw,"lxml")
 #print(soup.prettify())
 
 # extract the items from the HTML list
+
 listpics = soup.find_all("ul")
 print(len(listpics))
 listpics2 = listpics[0].find_all("li")
@@ -75,21 +109,27 @@ print(len(listpics2))
 # otherwise use the 250px square preview images
 # in the _files folder from the index HTML file
 
-# fullsize = True
-fullsize = False
+q = input("Do you have the full-size images (Y/N) in the current directory?")
+if q[0].upper() == "Y":
+	fullsize = True
+else:
+	fullsize = False
+	
 for p in listpics2:
+	# get the caption out of the HTML	
 	descript = p.a['data-description']
+	# the preview filename
 	prevfilename = p.a.img['src']
 	print(descript)
 	desclength = len(descript)
 	if fullsize:
-	        #expect the full-size images in the same directory as the script
+	    # expect the full-size images in the same directory as the script
 		mainfilename = os.path.split(prevfilename)[1]
 		try:
 			im = Image.open(mainfilename).convert('RGBA')
 		except IOError:
 			print("IOError encountered")
-			
+		# output at full resolution
 		out = createCaption(im, descript, 60)
 		print(prevfilename, mainfilename)
 		# resize to 1440px
@@ -101,8 +141,9 @@ for p in listpics2:
 		
 		# out.show()
 		# I encountered jpg problem in Pillow  - don't do conversion for now
-		# outfile = mainfilename.replace(".png", ".jpg")
-		# outfile = os.path.join(outdir, outfile)
+		#outfile = mainfilename.replace(".png", ".jpg")
+		#outfile = os.path.join(outdir, outfile)
+		#outfile2 = os.path.join(outdir, subdir, mainfilename.replace(".png", ".jpg"))
 		outfile = os.path.join(outdir, mainfilename)
 		outfile2 = os.path.join(outdir, subdir,mainfilename)
 
@@ -114,14 +155,15 @@ for p in listpics2:
 			except IOError:
 				print("cannot convert", mainfilename)
 	else:
-                # using the preview images
+        # using the preview images
+        # output file name
 		outfilename = os.path.split(prevfilename)[1]
 		try:
 			im = Image.open(prevfilename).convert('RGBA')
 		except IOError:
 			print("IOError encountered")
-                # for preview images, start a new line at every comma in caption
-                # and write it at the top-left
+        # for preview images, start a new line at every comma in caption
+        # and write it at the top-left
 		descript = descript.replace(",",",\n")
 		out = createCaption(im, descript, 16, left=True)		
 		print(prevfilename)
@@ -132,6 +174,3 @@ for p in listpics2:
 			except IOError:
 				print("cannot convert", prevfilename)		
 	#im.show()
-	
-	
-
