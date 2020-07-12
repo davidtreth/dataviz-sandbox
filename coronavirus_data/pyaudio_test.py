4922e22a57fa3669330337c3354a514fe3ef7fba
davidtreth <davidtreth@gmail.com>
2020-07-12 23:18:43 +0100

    vary duration of notes depending on difference between a days number of cases and the previous day

--- coronavirus_data/pyaudio_test.py
+++ coronavirus_data/pyaudio_test.py
@@ -4,11 +4,11 @@
 import corona_python_text
 import math
 import numpy
-import time
 from operator import itemgetter
 from pyaudio import PyAudio, paUInt8
 import wave
 from itertools import *
+import argparse
 
 # code partly copied from answers at https://stackoverflow.com/questions/974071/python-library-for-playing-fixed-frequency-sound
 
@@ -71,7 +71,7 @@ def generate_sine_wave(frequency, duration, volume=0.2, sample_rate=44100):
 notes = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
 
 
-def play_audio(cases_by_area, selected_area="", bass_octave = 3, range_octaves=4, scaling=1):    
+def play_audio(cases_by_area, selected_area="", bass_octave = 3, range_octaves=4, scaling=1, shorttext=False, duration=1):    
     bass_note = 261.63 / (5-bass_octave) # one octave below middle C if bass-octave is its default
     print("bass note = {h} Hz".format(h=bass_note))
     print("scaling = {s}. freq prop. to (cases/max cases)**scaling".format(s=scaling))
@@ -96,42 +96,76 @@ def play_audio(cases_by_area, selected_area="", bass_octave = 3, range_octaves=4
         #print(max(ncases_valslist))
         print("max cases = ",max_cases)
         
-        for n in zip(datelist, ncases_valslist):
+        for i, n in enumerate(zip(datelist, ncases_valslist)):
             # range of 4 octaves by default
             octaves = range_octaves*((n[1]/max_cases)**scaling)
             # quantise to the nearest semitone
             octaves = math.floor(octaves*12.0)/12.0
             # calculate frequency
             freq = bass_note*(2**octaves)
-            print("{d} {c} cases, {f:.3f} Hz, {n:.3f} octaves, {a}{b}".format(d=n[0], c=n[1], f=freq, n=octaves, a=notes[int((octaves*12) % 12)], b=int(bass_octave+math.floor(octaves))))
+            
+            if i > 0 and ncases_valslist[i-1]>0:
+                if ncases_valslist[i] == ncases_valslist[i-1]:
+                    duration_2 = duration / 4
+                elif abs((ncases_valslist[i]-ncases_valslist[i-1])/ncases_valslist[i-1]) < 0.1:
+                    duration_2 = duration / 2
+                else:
+                    duration_2 = duration
+            else:
+                duration_2 = duration
+            if duration_2 == duration:
+                note = "♩"
+            elif duration_2 == duration/2:
+                note = "♪"
+            elif duration_2 == duration/4:
+                note = "𝅘𝅥𝅯 "
+            else:
+                note = "♫"
+            if shorttext:
+                print("{a}{b}{s}".format(a=notes[int((octaves*12) % 12)], b=int(bass_octave+math.floor(octaves)), s=note), end=" ")
+            else:
+                print("{d} {c} cases, {f:.3f} Hz, {n:.3f} octaves, {a}{b}{s}".format(d=n[0], c=n[1], f=freq, n=octaves, a=notes[int((octaves*12) % 12)], b=int(bass_octave+math.floor(octaves)),s=note))
+                    
             generate_sine_wave(
             # see http://www.phy.mtu.edu/~suits/notefreqs.html
                 frequency=float(freq),   # Hz, waves per second C6
-                duration=1,       # seconds to play sound
+                duration=duration_2,       # seconds to play sound
                 volume=0.25,        # 0..1 how loud it is
                 sample_rate=44100,  # number of samples per second: 11025, 22050, 44100
             )
+            
         print("\n")
 
-# play the England cases, with square root scaling            
-cases_by_area = corona_python_text.cases_by_country
-play_audio(cases_by_area, "", 2, 5, 0.5)
+
+if __name__ == '__main__':
+    """
+    If invoked at the command-line
+    """
+    # Create the command line options parser.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--short",action="store_true",
+                        help="shorter form text output with just the note not date, cases, freq etc.")
+    args = parser.parse_args()
+    
+    # play the England cases, with square root scaling            
+    cases_by_area = corona_python_text.cases_by_country
+    play_audio(cases_by_area, "", 2, 5, 0.5, args.short)
 
 
-# play regions, with square root scaling
-cases_by_area = corona_python_text.cases_by_region
-play_audio(cases_by_area, "", 2, 5, 0.5)
+    # play regions, with square root scaling
+    cases_by_area = corona_python_text.cases_by_region
+    play_audio(cases_by_area, "", 2, 5, 0.5, args.short)
 
-# play upper-tier local authorities
-cases_by_area = corona_python_text.cases_by_utla
+    # play upper-tier local authorities
+    cases_by_area = corona_python_text.cases_by_utla
 
-# select only Cornwall
-# selectedarea = 'Cornwall and Isles of Scilly'
-# play without scaling
-# play_audio(cases_by_area, selectedarea)
+    # select only Cornwall
+    # selectedarea = 'Cornwall and Isles of Scilly'
+    # play without scaling
+    # play_audio(cases_by_area, selectedarea)
 
-# play with square-root scaling
-# play_audio(cases_by_area, selectedarea, scaling=0.5)
+    # play with square-root scaling
+    # play_audio(cases_by_area, selectedarea, scaling=0.5)
 
-# play all UTLAs with square-root scaling
-play_audio(cases_by_area, scaling=0.5)
+    # play all UTLAs with square-root scaling
+    play_audio(cases_by_area, "", 3, 4, 0.5, args.short)
