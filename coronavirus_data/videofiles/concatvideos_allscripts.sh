98dc3b876e27592252dc76523e4c8f9a8922a367
davidtreth <davidtreth@gmail.com>
2021-03-21 16:44:13 +0000

    update data to 20/03/2021

---
+++ coronavirus_data/videofiles/concatvideos_allscripts.sh
@@ -0,0 +1,11 @@
+sh concatvideos_short.sh
+sh concatvideos_NI.sh
+sh concatvideos_wales.sh
+sh concatvideos_scotland.sh
+sh concatvideos_London.sh
+sh concatvideos_eng.sh
+sh concatvideos_eng_ltla.sh
+sh concatvideos.sh
+python pygame_concatvideos_duration.py
+rm -v endframes/*png
+python ffmpeg_extract_endframes.py
